bdde2a044489d90809bd448dee653ee4668c651d
HuCutie <husmallcutie@gmail.com>
2024-02-03 00:22:29 +0800

    final

--- base.sh
+++ base.sh
@@ -3,9 +3,10 @@ apt install -y sudo git vim libgl1-mesa-glx libglib2.0-0 ntp wget tmux
 pip install -U openmim
 mim install mmcv-full==1.5.0
 
+python setup.py install
 pip install -r requirements.txt
 
-pip setup.py install
+pip install matplotlib
 pip install fairscale
 pip install scipy
 pip install timm
